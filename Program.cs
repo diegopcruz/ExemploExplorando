81404480b023b3c956d7999910750ca016b96fad
Diego Cruz <diegopcruz@outlook.com>
2023-11-12 17:58:11 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using ExemploExplorando.Models;
+
+Pessoa p1 = new Pessoa("Diego", "Pereira");
+
+Pessoa p2 = new Pessoa("Taila", "da Cruz");
+
+Curso cursoDeIngles = new Curso();
+cursoDeIngles.Nome = "Ingles";
+cursoDeIngles.Alunos = new List<Pessoa>();
+
+cursoDeIngles.AdicionarAluno(p1);
+cursoDeIngles.AdicionarAluno(p2);
+cursoDeIngles.ListarAlunos();
